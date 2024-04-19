fa1d02f81ee0242eb1629a8465b00ceae4fb4724
MarieSOWw <MarieSOWw@users.noreply.github.com>
2024-04-19 22:26:53 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for monprojet package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("monprojet", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="monprojet",
+    version=read("monprojet", "VERSION"),
+    description="Awesome monprojet created by MarieSOWw",
+    url="https://github.com/MarieSOWw/monprojet/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="MarieSOWw",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["monprojet = monprojet.__main__:main"]
     },
     extras_require={
         "test": read_requirements("requirements-test.txt")
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from monprojet import create_app
+from monprojet.ext.commands import populate_db
+from monprojet.ext.database import db
 
 
 @pytest.fixture(scope="session")
